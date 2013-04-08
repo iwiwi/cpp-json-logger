c1181a44725e74a093259dd474a6b5ee424248a9
Takuya Akiba <iw@iwi.tc>
2013-04-08 22:43:34 +0900

    Ignore non-json files

--- tools/jlog_rename.rb
+++ tools/jlog_rename.rb
@@ -4,32 +4,41 @@ require 'fileutils'
 require 'rubygems'
 require 'json'
 
-def new_name_json(j)
-  # Edit here!!
-  return j["indexing"]["algorithm"] + "_" +
-    j["setting"]["graph_file"].to_s.split("/")[-1]
+def new_name_json(j)  # j
+  return j["run"]["program"] + "_" + j["setting"]["graph"]
 end
 
 def new_name_file(f)
-  return new_name_json(JSON.parse(open(f).read))
+  begin
+    return new_name_json(JSON.parse(open(f).read))
+  rescue => e
+    $stderr.puts("#{f}: #{e.to_str}")
+    return f
+  end
 end
 
-def new_name_files(fs)
-  fs = fs.map{ |f| new_name_file(f) }
+def new_name_files(original_fs)
+  fs = original_fs.map{ |f| new_name_file(f) }
 
   cnt = (cnt2 = {}).dup
 
   fs.each{ |f| cnt[f] = cnt2[f] = 0 }
   fs.each{ |f| cnt[f] += 1 }
 
-  fs.each_with_index do |f, i|
-    if cnt[f] > 1
-      cnt2[f] += 1
-      fs[i] += "_" + cnt2[f].to_s
+  [fs, original_fs].transpose.each_with_index do |ff, i|
+    if ff[0] == ff[1]
+      # do nothing
+    else
+      f = ff[0]
+      if cnt[f] > 1
+        cnt2[f] += 1
+        fs[i] += "_" + cnt2[f].to_s
+      end
+      fs[i] += ".json"
     end
   end
 
-  return fs.map{ |f| f + ".json" }
+  return fs
 end
 
 if __FILE__ == $0
